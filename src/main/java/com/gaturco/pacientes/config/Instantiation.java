473067905f53a957fc4bdf28b7044d31868bf160
gaturco <turco.gabriel@live.com>
2019-10-20 23:02:55 -0300

    Instanciacao da base de dados

---
+++ src/main/java/com/gaturco/pacientes/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.gaturco.pacientes.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.gaturco.pacientes.domain.Paciente;
+import com.gaturco.pacientes.repository.PacienteRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private PacienteRepository pacienteRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		pacienteRepository.deleteAll();
+		
+		Paciente maria = new Paciente(null, "Maria Bronwn", "maria@gmail.com");
+		Paciente alex = new Paciente(null, "Alex Green", "Alex@gmail.com");
+		Paciente bob = new Paciente(null, "Bob Grey", "bob@gmail.com");
+		
+		pacienteRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
